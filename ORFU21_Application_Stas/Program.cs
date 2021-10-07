af35435cb473ecda9b1f5d36331b5ea022423c4d
Krezeb <lachlanmorrison@outlook.com>
2021-10-07 14:03:11 +0200

    Stuckers

--- ORFU21_Application_Stas/Program.cs
+++ ORFU21_Application_Stas/Program.cs
@@ -10,8 +10,7 @@ namespace ORFU21_Application_Stas
     {
         static void Main(string[] args)
         {
-            List<string> warehouseList = new List<string>() { "Wood", "Wood", "Steel" };
-            List<string> baseMaterialList = new List<string>() { "Wood", "Steel", "Glass", "Rubber" };
+            //List<string> baseMaterialList = new List<string>() { "Wood", "Steel", "Glass", "Rubber" };
 
             while (true)
             {
@@ -19,25 +18,33 @@ namespace ORFU21_Application_Stas
                 Console.WriteLine("------------Magiska Fabriken------------\n");
 
                 Console.WriteLine("Currently your warehouse holds:");
-                for (int i = 0; i < warehouseList.Count; i++)
-                {
-                    Console.WriteLine($"{warehouseList[i]}");
-                }
+                Warehouse showWarehouse = new Warehouse();
+                showWarehouse.WarehouseList();
 
-                Console.WriteLine("\nWhat Material Do you want to add?");
-                for (int i = 0; i < baseMaterialList.Count; i++)
-                {
-                    Console.WriteLine($"{i + 1}. {baseMaterialList[i]}");
-                }
-                
-                int baseMaterialInput = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine($"How many do you wish to add?");
-                int baseAmountInput = Convert.ToInt32(Console.ReadLine());
+                Warehouse addMaterialType = new Warehouse();
+                addMaterialType.AddMaterialType();
+                int typ = Convert.ToInt32(Console.ReadLine());
 
-                for (int i = 0; i < baseAmountInput; i++)
-                {
-                    warehouseList.Add(baseMaterialList[baseMaterialInput-1]);
-                }
+                Warehouse addMaterialNumber = new Warehouse();
+                addMaterialNumber.AddMaterialNumber();
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                Warehouse addNewMaterialToList = new Warehouse();
+                addNewMaterialToList.AddNewMaterial(typ, num);
+
+                //Console.WriteLine("\nWhat Material Do you want to add?");
+                //Warehouse showMaterials = new Warehouse();
+                //showWarehouse.BaseMaterialList();
+
+                //int baseMaterialInput = Convert.ToInt32(Console.ReadLine());
+
+                //Console.WriteLine($"How many do you wish to add?");
+                //int baseAmountInput = Convert.ToInt32(Console.ReadLine());
+
+                //for (int i = 0; i < baseAmountInput; i++)
+                //{
+                //    warehouseList.Add(baseMaterialList[baseMaterialInput - 1]);
+                //}
             }
         }
     }
--- ORFU21_Application_Stas/Warehouse.cs
+++ ORFU21_Application_Stas/Warehouse.cs
@@ -8,5 +8,44 @@ namespace ORFU21_Application_Stas
 {
     class Warehouse
     {
+        public List<string> warehouseList = new List<string>() { };
+        public List<string> baseMaterialList = new List<string>() { "Wood", "Steel", "Glass", "Rubber" };
+    public void WarehouseList()
+        {
+            for (int i = 0; i < warehouseList.Count; i++)
+            {
+                Console.WriteLine($"{warehouseList[i]}");
+            }
+        }
+        public void BaseMaterialList()
+        {
+            for (int i = 0; i < baseMaterialList.Count; i++)
+            {
+                Console.WriteLine($"[{i+1}] {baseMaterialList[i]}");
+            }
+        }
+        
+        public void AddMaterialType()
+        {
+            Console.WriteLine("\nWhat Material Do you want to add?");
+            for (int i = 0; i < baseMaterialList.Count; i++)
+            {
+                Console.WriteLine($"[{i + 1}] {baseMaterialList[i]}");
+            }
+        }
+
+        public void AddMaterialNumber()
+        {
+            Console.WriteLine("\nHow Many?");
+        }
+
+        public void AddNewMaterial(int baseMaterialAmountInput, int baseMaterialInput)
+        {
+            for (int i = 0; i < baseMaterialAmountInput; i++)
+            {
+                warehouseList.Add(baseMaterialList[baseMaterialInput - 1]);
+            }
+        }
+
     }
 }
